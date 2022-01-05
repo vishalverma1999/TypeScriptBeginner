7f18c336658d3e6c6e63bec353e219d9167329b6
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-01-05 06:42:49 +0530

    TypeScript Tutorial #3 - Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,23 @@
-// since browser does not understand ts hence we must compile down it to js
-// To compile ts file to js, in terminal write, (tsc xyz.ts abc.js) and for continuous compilation use (tsc xyz.ts abc.js -w) ,  where w stands for watch
-// now when the output file is named the same as the input file i.e. sandbox and sandbox then we can just take off this last bit i.e. (abc.js) and typescript automatically compiles it to a file with the same name but just with a javascript extension
-var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// fundamental difference between typescript and JavaScript is that typescript uses strict types, that means if we define a variable as a string then in typescript it will always be a string and it's type cannot be changed later and the same would be true of any other type like numbers, booleans, objects, arrays
+// we don't have to specifically say the character is going to be a string typescript uses what's known as inference or it infers the type based on the value we assign it, although we can explicitly provide the type of variable
+
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+
+// character = 20;    // Error- Type 'number' is not assignable to type 'string'
+character = 'vishal';
+
+// age = 'yoshi';       // Error- Type 'string' is not assignable to type 'number'
+age = 20;
+
+// isBlackBelt = 'yes';    // Error
+isBlackBelt = true;
+
+
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+
+// console.log(circ('hello'));    // No error in js whatsoever because js follows dynamic datatype rule therefore no type checking will be done, because typecheck done at compile time not at run time
+console.log(circ(5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,23 @@
-// since browser does not understand ts hence we must compile down it to js
-// To compile ts file to js, in terminal write, (tsc xyz.ts abc.js) and for continuous compilation use (tsc xyz.ts abc.js -w) ,  where w stands for watch
-// now when the output file is named the same as the input file i.e. sandbox and sandbox then we can just take off this last bit i.e. (abc.js) and typescript automatically compiles it to a file with the same name but just with a javascript extension
-const character = 'luigi';
+// fundamental difference between typescript and JavaScript is that typescript uses strict types, that means if we define a variable as a string then in typescript it will always be a string and it's type cannot be changed later and the same would be true of any other type like numbers, booleans, objects, arrays
+// we don't have to specifically say the character is going to be a string typescript uses what's known as inference or it infers the type based on the value we assign it, although we can explicitly provide the type of variable
 
-console.log(character);
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
-const inputs = document.querySelectorAll('input');
+// character = 20;    // Error- Type 'number' is not assignable to type 'string'
+character = 'vishal';
 
-inputs.forEach(input=> {
-    console.log(input);
-})
+// age = 'yoshi';       // Error- Type 'string' is not assignable to type 'number'
+age = 20;
+
+// isBlackBelt = 'yes';    // Error
+isBlackBelt =true;
+
+const circ = (diameter: number)=>{              // diameter: number, passing paameter is diameter of type number
+
+    return diameter*Math.PI;
+}
+
+// console.log(circ('hello'));    // right now we can pass anything to diameter and it will not throw error, but once we defines the type of diameter then the passed value must match type 
+console.log(circ(5)); 

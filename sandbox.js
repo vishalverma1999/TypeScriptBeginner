43a431659189d3c82605aa4947b370fefe5f49d0
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-01-05 07:47:26 +0530

    TypeScript Tutorial #5 - Explicit Types

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,34 @@
+// explicit types
+var character = 'vishal';
+var age;
+var isLoggedIn;
+// age = 'luigi';    // type error
+age = 30;
+// isLoggedIn = 25;
+isLoggedIn = true;
 // arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('vishal'); // ok
-// names.push(3);  // Error- Argument of type 'number' is not assignable to parameter of type 'string'
-// names[0] = 5;   // error
-var numbers = [10, 20, 12, 15];
-numbers.push(25); // ok 
-// numbers.push('shaun');      // error
-// numbers[0] = 'shaun';       // error
-var mixed = ['ken', 4, 'chun-li', 8, 9]; //  typescript will look at this and say okay well this is an array now which can contain strings or numbers
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-// objects- same as js object but once the type of properties is decided then it can't be change like propert name has type string
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40; //ok
-ninja.name = 'ryu'; //ok
-// ninja.age = '30';     // type Error
-// ninja.skills = ['fighting', 'sneaking'];   // we can't just add on extra properties once we've defined the variable or the object to begin with so I can't say ninja and then make up a new property called skills and set that equal to an array of some kind of skills like fighting or maybe sneaking I can't do this because this property skills did not exist on the ninja object to begin with and once we've defined the object we then can't add additional properties to it
-// BUT we can overwrite the whole object by creating object only and not by dot or assignment operator
-// During the overwrite you can't change the initial stucture i.e. the total number of properties and there types, but you can change the there values as shown below 
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40
-};
+var strArray; // arroy of strings only
+// strArray.push('vishal');  // error because at the time strArray is not declared, we tell that in future whenever it is declared must be of string type
+var ninjas = []; // Empty Array Initialized, here we can use push method since empty array is initialized
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
+// union types - one or more than one types
+var mixed = []; // empty array initialized with type string, number and boolean
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
+// We can use union types on normal variables as well
+var uid; //uid can be string or number
+uid = '123';
+uid = 123;
+console.log(uid);
+// object
+var ninjaOne; // ninjaone is object, array is also an object
+ninjaOne = { name: 'yoshi', age: 30 }; // object using curly braces
+ninjaOne = ['vkjdnvd', true, 3543]; // array is also an object
+console.log(ninjaOne);
+// Here ninnjaTwo is the object with curly braces only with explicitly definesd it's property types
+var ninjaTwo;
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
--- sandbox.ts
+++ sandbox.ts
@@ -1,40 +1,48 @@
+// explicit types
+let character: string = 'vishal';   
+let age: number;
+let isLoggedIn: boolean;
+
+// age = 'luigi';    // type error
+age = 30;
+
+// isLoggedIn = 25;
+isLoggedIn = true;
+
+
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
+let strArray: string[];   // arroy of strings only
+// strArray.push('vishal');  // error because at the time strArray is not declared, we tell that in future whenever it is declared must be of string type
 
-names.push('vishal');   // ok
-// names.push(3);  // Error- Argument of type 'number' is not assignable to parameter of type 'string'
-// names[0] = 5;   // error
+let ninjas: string[] = [];   // Empty Array Initialized, here we can use push method since empty array is initialized
 
-let numbers = [10, 20, 12, 15];
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
 
-numbers.push(25);   // ok 
-// numbers.push('shaun');      // error
-// numbers[0] = 'shaun';       // error
+// union types - one or more than one types
+let mixed: (string|number|boolean)[] = [];    // empty array initialized with type string, number and boolean
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
 
-let mixed = ['ken', 4, 'chun-li', 8, 9];   //  typescript will look at this and say okay well this is an array now which can contain strings or numbers
+// We can use union types on normal variables as well
+let uid: string|number;   //uid can be string or number
+uid = '123';
+uid = 123;
+console.log(uid);
 
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+// object
+let ninjaOne: object;   // ninjaone is object, array is also an object
+ninjaOne = { name: 'yoshi', age: 30 };   // object using curly braces
+ninjaOne = ['vkjdnvd', true, 3543];    // array is also an object
+console.log(ninjaOne);
 
-
-// objects- same as js object but once the type of properties is decided then it can't be change like propert name has type string
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-  };
-  
-  ninja.age = 40;  //ok
-  ninja.name = 'ryu';  //ok
-// ninja.age = '30';     // type Error
-// ninja.skills = ['fighting', 'sneaking'];   // we can't just add on extra properties once we've defined the variable or the object to begin with so I can't say ninja and then make up a new property called skills and set that equal to an array of some kind of skills like fighting or maybe sneaking I can't do this because this property skills did not exist on the ninja object to begin with and once we've defined the object we then can't add additional properties to it
-
-// BUT we can overwrite the whole object by creating object only and not by dot or assignment operator
-// During the overwrite you can't change the initial stucture i.e. the total number of properties and there types, but you can change the there values as shown below 
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40,
-    // skills: ['running'],
-  };
+// Here ninnjaTwo is the object with curly braces only with explicitly definesd it's property types
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string
+};
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };

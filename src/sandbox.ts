8d3f19e808bc762d0285a6f0a11341e9779d8342
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-01-05 23:26:05 +0530

    TypeScript Tutorial #10 - Function Signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,10 +1,34 @@
-type StringOrNum = string|number;    // type alias
-type ObjWithName = {name: string, uid: StringOrNum };  // type alias
+// example 1
+let greet: (a: string, b: string) => void;     // This is called function signature having two parameters a and b with type string and it returns nothing i.e void
 
-const logDetails = (uid: StringOrNum, item: string)=> {
-        console.log(`${item} has a uid of ${uid}`);
+// creating Function with function signature
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 }
 
-const greet = (user: ObjWithName)=> {
-    console.log(`${user.name} says hello`);
+console.log(greet('vishal', 'hello'));    // passing the value
+
+// example 2
+let calc: (a: number, b: number, c: string) => number;  // return type is number
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
 }
+console.log(calc(50, 20, 'add'));
+
+
+// example 3
+let logDetails: (obj: {name: string, age: number}) => void;    // In this case Object is the Parameter 
+
+// type person = {name: string, age: number};   // we can also do the alias
+
+logDetails = (ninja: {name: string, age: number}) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+}
+
+console.log(logDetails({name: 'Tanny_Tiddi', age: 12}));
+

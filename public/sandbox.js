0991122f531f3037f19ec6e5d8f5bc838462203c
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-01-05 19:32:29 +0530

    TypeScript Tutorial #9 Type Aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,31 +1,7 @@
 "use strict";
-let greet = () => {
-    console.log("hello, world");
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-// typescript will automatically infer that the type of greet variable is going to be a function much like in other types
-// greet = 'hello';    // Error- Type 'string' is not assignable to type '() => void'
-// instead of defining a value for a specific variable which will be a function we don't have to do that we can explicitly say that this will be a function in the future much like we did with other types
-let calbert;
-calbert = () => {
-    console.log("hello again");
+const greet = (user) => {
+    console.log(`${user.name} says hello`);
 };
-// -------------------------------------------------------------------------------------------------------------------
-const add = (a, b, c) => {
-    console.log(a + b);
-    console.log(c); // so when we don't pass it through the value, the optional parameter that is becomes undefined
-};
-add(5, 10);
-// With default parameter
-// when we use a default value we don't need this optional parameter because we're saying okay well it's still going to be optional but the default parameter is going to be 10 if they don't pass  something in so we don't tend to use the question mark and the default parameter together just one or the other
-const multiply = (a, b, c = 20) => {
-    console.log(a * b);
-    console.log(c);
-};
-multiply(5, 10);
-// Returning Value
-const minus = (a, b) => {
-    return a - b; // typescript can infer the type of what this arrow function is returning
-};
-let result = minus(10, 7); // type of reult is a number because it's inferred the return type of minus function
-//   result = 'something else';   // error - since the type of result is number, hence typeError
-console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,48 +1,10 @@
-let greet = ()=> {
-    console.log("hello, world");
-}
-// typescript will automatically infer that the type of greet variable is going to be a function much like in other types
+type StringOrNum = string|number;    // type alias
+type ObjWithName = {name: string, uid: StringOrNum };  // type alias
 
-// greet = 'hello';    // Error- Type 'string' is not assignable to type '() => void'
-
-
-
-// instead of defining a value for a specific variable which will be a function we don't have to do that we can explicitly say that this will be a function in the future much like we did with other types
-
-let calbert: Function;
-
-calbert = ()=> {
-    console.log("hello again");
+const logDetails = (uid: StringOrNum, item: string)=> {
+        console.log(`${item} has a uid of ${uid}`);
 }
 
-// -------------------------------------------------------------------------------------------------------------------
-
-const add = (a:number, b:number, c?: number|string)=>{    // if I want c, to be optional all I have to do is place a question mark in front of it
-    console.log(a+b);
-    console.log(c);  // so when we don't pass it through the value, the optional parameter that is becomes undefined
+const greet = (user: ObjWithName)=> {
+    console.log(`${user.name} says hello`);
 }
-
-add(5,10);
-
-
-// With default parameter
-// when we use a default value we don't need this optional parameter because we're saying okay well it's still going to be optional but the default parameter is going to be 10 if they don't pass  something in so we don't tend to use the question mark and the default parameter together just one or the other
-
-const multiply = (a:number, b:number, c: number|string=20): void=>{    // return type void which means when we don't actually return something,  IMPORTANT!!- now when this is compiled into JavaScript that will become undefined but in typescript void is completely separate to undefined
-
-    console.log(a*b);
-    console.log(c);  
-}
-
-multiply(5,10);
-
-
-// Returning Value
-const minus = (a: number, b: number): number => {   // we can explicitly declare the type or return type of a function, here it is number written after the parenthesis, although we don't need to do that because typescript is automatically inferring the return type 
-    return a - b;    // typescript can infer the type of what this arrow function is returning
-  }
-  
-  let result = minus(10,7);     // type of reult is a number because it's inferred the return type of minus function
-//   result = 'something else';   // error - since the type of result is number, hence typeError
-
-  console.log(result);

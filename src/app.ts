9ef6198a43729dee26def7da9de23e3c9cda9a86
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-01-06 19:02:39 +0530

    TypeScript Tutorial #12 - Classes

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,46 @@
-// Almost same as js but there are few key differences
+// Classes
+class Invoice {
+    // Different properties that invoice object would have, when we create invoice object from class
+    // Either you need to directly assign the below properties like-  client: string = 'Vishal'  -or assign it with the help of contructor done below, else error will be thrown
+    client: string;
+    details: string;
+    amount: number;
 
-// const anchor = document.querySelector('a')!;
+    // constructor invoked immediately after the object is ceated from class
+    constructor(c: string, d: string, a: number) {
+        this.client = c;     // this referes to the instance of the object that we will be creating using invoice class
+        this.details = d;
+        this.amount = a;
+    }
 
-// console.log(anchor);   // works fine
-// console.log(anchor.href);   // if I try now to access a property of anchor such as href which we can do, but here we get some kind of error, which on hover says that this - object is possibly "null". So it's saying well this right here might actually be null and the reason it's saying that is because typescript doesn't actually know whether there is an anchor tag in the index page during developments. It doesn't have some kind of special access to the index page to be able to find that out so it's just warning us here that look this could be null and if this is null then you're not going to get an href property on it
+    format(){
+        // returning a simple string which is formatting the different properties
+        return `${this.client} owes INR${this.amount} for ${this.details}`;
+    }
+
+}
+
+// creating object from class
+const invOne = new Invoice('vishal', 'kdhnksjbkc', 400);
+const invTwo = new Invoice('Tanny', 'blah blah blah', 100);
+
+// by default all properties of class are public, so we can change it once we instantiated an object from the class
+invOne.client = 'MunnuDada';
+invOne.amount = 50;
+
+console.log(invOne, invTwo);
+
+// now the cool thing about using these classes for objects is that if we wanted to in the future we could create maybe an array and only allow invoice objects in the array much like we did string arrays or boolean arrays where only that type is allowed in the array, we can do that with classes or class objects as well
+// const invoices: string[] = [];   // just like we did this, making an empty array of type string only, we can do that with class also
+const invoices: Invoice[] = [];     // array of type Invoice class
+// invoices.push('hello');    // Not allowed
+// invoices.push({name: 'vishal'});    // Not allowed
+invoices.push(invOne);     // allowed
+invoices.push(invTwo);     // allowed
+
+console.log(invoices);
 
 
-// so we can combat this in one of two different ways FIRST we could do some kind of a RUNTIME check
-// if(anchor){
-//     console.log(anchor.href);
-// }
-
-// SECOND - if we are certain as developers that this thing exists inside our HTML file then add on an exclamation mark to the end of that query selector
-// console.log(anchor.href);
-
-// for Dom interaction typescript automatically contains special types for every Dom elements so if I hover over anchor we can see that Const anchor is an HTML anchor element
-
-
-//------------------------------------------------------------------------------------------------------------------------------------------
-// const form = document.querySelector('form')!;   // type of form variable is -  HTMLFormElement
-
-// const form = document.querySelector('new-item-form')!;  // type : Element, because when we use form or anchor tag typescript knows what tag we're grabbing now in this case we're just passing a class and a class could be applied to any different element in the HTML page so it doesn't know that it's a form it just knows that it's going to be some kind of elements . We can overome by Type Casting
 
 // TypeCasting
 const form = document.querySelector('.new-item-form') as HTMLFormElement;  // since we use 'as' keyword then it's never going to be null, now we get all of the right properties and methods available to us in some form of intellisense inside the vs code
@@ -33,7 +53,7 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
 
     e.preventDefault();
     console.log(
@@ -43,3 +63,14 @@ form.addEventListener('submit', (e)=>{
         amount.valueAsNumber,       // javascript by default turning the number to string, so to prevent this instead just using amount.value use amount.valueAsNumber
     )
 })
+
+
+
+
+
+
+
+
+/*
+
+ */

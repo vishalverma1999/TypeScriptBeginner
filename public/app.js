759766bd8969522d5d0e0c31165335f55a2f7b79
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-01-07 21:54:41 +0530

    TypeScript Tutorial #18 - Generics

--- public/app.js
+++ public/app.js
@@ -22,8 +22,45 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
-/*
-Two Drawbacks Of Using Module:-
-1) Supported by modern browsers only
-2) it doesn't bundle our code into a single file the browser is still using the module system to load separate files(here in our project app.js and invoice.js) and make multiple requests i.e for app.js and invoice.js
- */
+// Generics- Allows Us to create Reusable blocks of code which can be used with different types
+// const addUID = (obj: Object) => {     // here arrow function takes an obj which is any kind of object with any property
+//     let uid = Math.floor((Math.random()*100));
+//     return {...obj, uid};     // properties destructured from obj and extra uid added to it
+// }
+// let docOne = addUID({name:'yoshi', age: 40});
+// console.log(docOne.name);   // Error - if I try to access a property on this docOne like the name which I should be able to do we get an error and that's because it says property name does not exist on this type with a UID property , now why is that well it's because when we pass in an object into this function right we're not specifying exactly what this object should be right and it doesn't know when it returns this new object right here what properties were on the object that we passed in so it doesn't know that a name exists on the object or that an age exists on the object because we've not said that anywhere and it's not captured those inside the function so when it returns this it doesn't know what properties it's going to output right here 
+// ABOVE PROBLEM CAN BE SOLVED IF WE USE GENERIC
+// const addUID = <T>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+// let docOne = addUID({name:'yoshi', age: 40});
+// let docTwo = addUID('hello');   // No sense since random id can't be attached to it BUT This will work fine because type of T or generic is not specified but it will just capture the type and it's specifications
+// console.log(docOne.name);   // ERROR GONE
+// Specifying the type of Generic
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+// let docOne = addUID({name:'yoshi', age: 40});
+// console.log(docOne.age);
+//We can be more specific in specifying the type of Generic
+// now it's only going to allow in objects which have a name property and that name has to be a string
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 }); // This is fine since it contains name property, extra is ok
+console.log(docOne.name);
+//   defining the resource object
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 1,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk']
+};
+console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,68 @@ form.addEventListener('submit', (e) => {
 })
 
 
-/*
-Two Drawbacks Of Using Module:-
-1) Supported by modern browsers only
-2) it doesn't bundle our code into a single file the browser is still using the module system to load separate files(here in our project app.js and invoice.js) and make multiple requests i.e for app.js and invoice.js 
- */
+// Generics- Allows Us to create Reusable blocks of code which can be used with different types
+
+// const addUID = (obj: Object) => {     // here arrow function takes an obj which is any kind of object with any property
+//     let uid = Math.floor((Math.random()*100));
+//     return {...obj, uid};     // properties destructured from obj and extra uid added to it
+// }
+
+// let docOne = addUID({name:'yoshi', age: 40});
+// console.log(docOne.name);   // Error - if I try to access a property on this docOne like the name which I should be able to do we get an error and that's because it says property name does not exist on this type with a UID property , now why is that well it's because when we pass in an object into this function right we're not specifying exactly what this object should be right and it doesn't know when it returns this new object right here what properties were on the object that we passed in so it doesn't know that a name exists on the object or that an age exists on the object because we've not said that anywhere and it's not captured those inside the function so when it returns this it doesn't know what properties it's going to output right here 
+
+
+// ABOVE PROBLEM CAN BE SOLVED IF WE USE GENERIC
+// const addUID = <T>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+// let docOne = addUID({name:'yoshi', age: 40});
+// let docTwo = addUID('hello');   // No sense since random id can't be attached to it BUT This will work fine because type of T or generic is not specified but it will just capture the type and it's specifications
+// console.log(docOne.name);   // ERROR GONE
+
+
+// Specifying the type of Generic
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+// let docOne = addUID({name:'yoshi', age: 40});
+// console.log(docOne.age);
+
+//We can be more specific in specifying the type of Generic
+// now it's only going to allow in objects which have a name property and that name has to be a string
+const addUID = <T extends { name: string }>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return { ...obj, uid };
+}
+
+let docOne = addUID({ name: 'yoshi', age: 40 });   // This is fine since it contains name property, extra is ok
+console.log(docOne.name);
+
+
+// with interfaces
+interface Resource<T> {   // We are going to pass in the type when we define a resourec object
+    uid: number;
+    resourceName: string;
+    data: T;     // we want data property to be flexible means it may be string may be object, therefore we used generic
+}
+
+//   defining the resource object
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+
+const docFour: Resource<string[]> = {
+    uid: 1, 
+    resourceName: 'shoppingList', 
+    data: ['bread', 'milk']
+  };
+  
+  console.log(docThree, docFour);
+
+
